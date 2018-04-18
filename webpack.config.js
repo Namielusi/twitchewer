c271be2f681e1bb776e763dbad86fa41cd18d012
Namielusi <namielusi@gmail.com>
2018-04-18 08:00:50 +0300

    Add init structure with react, redux, webpack, babel and linter

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const webpack = require('webpack');
+
+module.exports = {
+  entry: [
+    'react-hot-loader/patch',
+    './app/index.js',
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader', 'eslint-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
+  output: {
+    path: `${__dirname}/app/dist`,
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+  devServer: {
+    contentBase: './app/static',
+    hot: true,
+  },
+};
