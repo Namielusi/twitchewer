956d564fb6a3730d58eb35618e9c289545860484
Vyacheslav Kostenko <namielusi@gmail.com>
2018-05-14 04:25:30 +0300

    Fix port

--- app/server.js
+++ app/server.js
@@ -10,7 +10,7 @@ if (process.env.NODE_ENV !== 'production') {
   app.use(webpack);
 }
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'static')));
 
